aec34b5a0b20cf61f54fd33862c2dc683898e3fc
Vlad <vmusaelyae@gmail.com>
2023-09-23 00:49:08 +0300

    Added some pages and urls

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.landing_page),
+    path('', views.landing_page, name='landing'),
+    path('products/', views.products_page, name='products'),
+    path('shoppinglist/', views.shoppinglist_page, name='shoppinglist'),
+    path('aboutus/', views.aboutus_page, name='aboutus'),
+    path('contacts/', views.contacts_page, name='contacts'),
 ]
--- products/views.py
+++ products/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 def landing_page(request):
     return render(request, 'products/landing_page.html')
+
+
+def products_page(request):
+    return render(request, 'products/products_page.html')
+
+
+def aboutus_page(request):
+    return render(request, 'products/aboutus_page.html')
+
+
+def contacts_page(request):
+    return render(request, 'products/aboutus_page.html')
+
+
+def shoppinglist_page(request):
+    return render(request, 'products/shoppinglist_page.html')
+
+
+def contacts_page(request):
+    return render(request, 'products/contacts_page.html')
