8fb9a02cae7d8d08e838c171e54a7bdec7e53ef6
Vlad <vmusaelyae@gmail.com>
2023-09-23 12:13:51 +0300

    Added logic login

--- users/views.py
+++ users/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+
+from models import User
+from users.forms import UserLoginForm
 
 # Create your views here.
 def login(request):
-    return render(request, 'users/login_page.html')
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return redirect('products:landing')
+    else:
+        form = UserLoginForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'users/login_page.html', context)
 
 def register(request):
     return render(request, 'users/register_page.html')
