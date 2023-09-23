d838ac423a7fd1cc9d9f6ecdb67e510e80a22f09
Vlad <vmusaelyae@gmail.com>
2023-09-23 13:01:40 +0300

    Added forms

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,13 @@
+from django import forms
+
 from django.contrib.auth.forms import AuthenticationForm
 
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput())
+    password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
         fields = ('username', 'password')
