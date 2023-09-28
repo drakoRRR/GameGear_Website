c20a105cc21d24d72190a30f151268a3c34caa60
Vlad <vmusaelyae@gmail.com>
2023-09-28 11:32:23 +0300

    Refactor code, moved the logic of pagination into a separate function, added constant for pagination

--- products/views.py
+++ products/views.py
@@ -8,7 +8,11 @@ from django.views.generic.list import ListView
 from .models import ProductCategory, Product, Basket
 
 # Create your views here.
+PER_PAGE = 6  # Constant for pagination
+
 class LandingView(ListView):
+    '''Landing page'''
+
     model = Product
     template_name = 'products/landing_page.html'
 
@@ -21,6 +25,115 @@ class LandingView(ListView):
         context['categories'] = ProductCategory.objects.all()
         return context
 
+
+class AboutUsView(TemplateView):
+    '''About Us page'''
+    template_name = 'products/aboutus_page.html'
+
+
+class ContactsView(TemplateView):
+    '''Contacts page'''
+    template_name = 'products/contacts_page.html'
+
+
+@login_required
+def basket_add(request, product_id):
+    '''Logic of adding a product to basket'''
+
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+@login_required
+def basket_remove(request, basket_id):
+    '''Logic of removing a product to basket'''
+
+    basket = Basket.objects.filter(id=basket_id)
+    basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+def search_paginate(products, sort_option, page):
+    '''Helper function to get paginate pages'''
+
+    sorting_options = {
+        'alphabet': 'name',
+        'low-high': 'price',
+        'high-low': '-price',
+    }
+
+    sort_field = sorting_options.get(sort_option)
+    if sort_field:
+        products = products.order_by(sort_field)
+
+    paginator = Paginator(products, PER_PAGE)
+    products_paginator = paginator.page(page)
+
+    return products_paginator
+
+
+def product_list(request, category_id=None, page=1):
+    '''Product list page'''
+
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+    sort_option = request.GET.get('sorting-products')
+
+    context = {
+        'categories': ProductCategory.objects.all(),
+        'products': search_paginate(products, sort_option, page),
+        'category_id': category_id,
+        'current_sorting_option': request.GET.get('sorting-products', 'default'),
+    }
+
+    return render(request, 'products/products_page.html', context)
+
+def sort(request, category_id=None, page=1):
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+    sort_option = request.GET.get('sorting-products')
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        products = products.filter(name__icontains=search_query)
+
+    context = {
+        'categories': ProductCategory.objects.all(),
+        'products': search_paginate(products, sort_option, page),
+        'category_id': category_id,
+        'page': page,
+        'search_query': search_query,
+        'current_sorting_option': request.GET.get('sorting-products', 'default'),
+    }
+
+    return render(request, 'products/products_page.html', context)
+
+
+def search(request, page=1):
+    '''Logic of search'''
+
+    search_query = request.GET.get('search')
+    sort_option = request.GET.get('sorting-products')
+
+    if search_query is None:
+        products = Product.objects.all()
+    else:
+        products = Product.objects.filter(name__icontains=search_query)
+
+    context = {
+        'categories': ProductCategory.objects.all(),
+        'products': search_paginate(products, sort_option, page),
+        'search_query': search_query,
+        'current_sorting_option': sort_option,
+        'flag_search': True
+    }
+
+    return render(request, 'products/products_page.html', context)
 
 # class ProductsView(ListView):
 #     model = Product
@@ -62,129 +175,3 @@ class LandingView(ListView):
 #         if self.request.GET.get('search') is not None or self.request.GET.get('sorting-products') is not None:
 #             return None
 #         return super().get_paginate_by(queryset)
-
-
-class AboutUsView(TemplateView):
-    template_name = 'products/aboutus_page.html'
-
-
-class ContactsView(TemplateView):
-    template_name = 'products/contacts_page.html'
-
-
-@login_required
-def basket_add(request, product_id):
-    product = Product.objects.get(id=product_id)
-    baskets = Basket.objects.filter(user=request.user, product=product)
-
-    if not baskets.exists():
-        Basket.objects.create(user=request.user, product=product, quantity=1)
-    else:
-        basket = baskets.first()
-        basket.quantity += 1
-        basket.save()
-
-    return HttpResponseRedirect(request.META['HTTP_REFERER'])
-
-@login_required
-def basket_remove(request, basket_id):
-    basket = Basket.objects.filter(id=basket_id)
-    basket.delete()
-    return HttpResponseRedirect(request.META['HTTP_REFERER'])
-
-
-def product_list(request, category_id=None, page=1):
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    sort_option = request.GET.get('sorting-products')
-
-    sorting_options = {
-        'alphabet': 'name',
-        'low-high': 'price',
-        'high-low': '-price',
-    }
-
-    sort_field = sorting_options.get(sort_option)
-    if sort_field:
-        products = products.order_by(sort_field)
-
-    per_page = 6
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page)
-
-    context = {
-        'categories': ProductCategory.objects.all(),
-        'products': products_paginator,
-        'category_id': category_id,
-        'current_sorting_option': request.GET.get('sorting-products', 'default'),
-    }
-
-    return render(request, 'products/products_page.html', context)
-
-def sort(request, category_id=None, page=1):
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    sort_option = request.GET.get('sorting-products')
-    search_query = request.GET.get('search', '')
-
-    if search_query:
-        products = products.filter(name__icontains=search_query)
-
-    sorting_options = {
-        'alphabet': 'name',
-        'low-high': 'price',
-        'high-low': '-price',
-    }
-
-    sort_field = sorting_options.get(sort_option)
-    if sort_field:
-        products = products.order_by(sort_field)
-
-    per_page = 6
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page)
-
-    context = {
-        'categories': ProductCategory.objects.all(),
-        'products': products_paginator,
-        'category_id': category_id,
-        'page': page,
-        'search_query': search_query,
-        'current_sorting_option': request.GET.get('sorting-products', 'default'),
-    }
-
-    return render(request, 'products/products_page.html', context)
-
-
-def search(request, page=1):
-    search_query = request.GET.get('search')
-    sort_option = request.GET.get('sorting-products')
-
-    if search_query is None:
-        products = Product.objects.all()
-    else:
-        products = Product.objects.filter(name__icontains=search_query)
-
-    sorting_options = {
-        'alphabet': 'name',
-        'low-high': 'price',
-        'high-low': '-price',
-    }
-
-    sort_field = sorting_options.get(sort_option)
-    if sort_field:
-        products = products.order_by(sort_field)
-
-    per_page = 6
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page)
-
-    context = {
-        'categories': ProductCategory.objects.all(),
-        'products': products_paginator,
-        'search_query': search_query,
-        'current_sorting_option': sort_option,
-        'flag_search': True
-    }
-
-    return render(request, 'products/products_page.html', context)
-
-
