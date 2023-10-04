ba28d48d938bb1cddc4b40a3649f1f084e34a4b7
Vlad <vmusaelyae@gmail.com>
2023-10-05 00:22:47 +0300

    Added stripe base function

--- orders/urls.py
+++ orders/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'orders'
 
 urlpatterns = [
-    path('create/', views.OrderCreateView.as_view(), name='create_order')
+    path('order-create/', views.OrderCreateView.as_view(), name='create_order'),
+    path('order-success/', views.SuccessView.as_view(), name='success_order'),
+    path('order-canceled/', views.CanceledView.as_view(), name='canceled_order'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,15 +1,46 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+import stripe
 
+from http import HTTPStatus
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, TemplateView
+
+from gamegearsite import settings
 from orders.forms import OrderForm
 
 
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
 # Create your views here.
+class SuccessView(TemplateView):
+    template_name = 'orders/order_success_page.html'
+
+
+class CanceledView(TemplateView):
+    template_name = 'orders/canceled.html'
+
+
 class OrderCreateView(CreateView):
     template_name = 'orders/order_page.html'
     form_class = OrderForm
     success_url = reverse_lazy('orders:create_order')
+
+    def post(self, request, *args, **kwargs):
+        super(OrderCreateView, self).post(request, *args, **kwargs)
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    'price': 'price_1NxcJpLhQJgpKyIBqxBCl8ib',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:success_order')),
+            cancel_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:canceled_order')),
+        )
+        return HttpResponseRedirect(checkout_session.url, status=HTTPStatus.SEE_OTHER)
 
     def form_valid(self, form):
         form.instance.initiator = self.request.user
